29f1cbf1499285b5b1919168043644271568d316
Copperbutton <buptzxk2012@gmail.com>
2014-09-12 12:03:05 -0700

    Fixed Next Integer Bug

--- NextInteger.java
+++ NextInteger.java
@@ -1,12 +1,11 @@
 /**
- Write a function return an integer that satisfies the following conditions:
- 1) positive integer
- 2) no repeated digits, eg., 123 (valid), 122 (invalid)
- 3) incremental digit sequence, eg., 1234 (valid) 1243(invalid)
- 4) the returned integer MUST be the smallest one that greater than the input. eg., input=987, return=1023
-
- function signature could be like this:
- String nextInteger(String input)
+ * Write a function return an integer that satisfies the following conditions:
+ * 1) positive integer 2) no repeated digits, eg., 123 (valid), 122 (invalid) 3)
+ * incremental digit sequence, eg., 1234 (valid) 1243(invalid) 4) the returned
+ * integer MUST be the smallest one that greater than the input. eg., input=987,
+ * return=1023
+ * 
+ * function signature could be like this: String nextInteger(String input)
  */
 package interviewquestions.google;
 
@@ -17,7 +16,8 @@ public class NextInteger {
         int len = inte.length();
         char[] chs = inte.toCharArray();
         char[] res = new char[len + 1];
-        if (findNextInteger(chs, res, 0, false))
+        if (len > 0 && chs[0] + len - 1 <= '9'
+                && findNextInteger(chs, res, 0, false))
             return new String(Arrays.copyOfRange(res, 1, len + 1));
         for (int i = 0; i < len + 1; i++)
             res[i] = (char) ('1' + i);
@@ -29,9 +29,13 @@ public class NextInteger {
         if (index == chs.length) {
             return true;
         }
-        char base = incOrder ? (char) (res[index] + 1) : chs[index];
-        if (!incOrder && index == chs.length - 1)
-            base++;
+        char base = (char) (res[index] + 1);
+        if (!incOrder) {
+            base = (char) Math.max(base, chs[index]);
+            if (index == chs.length - 1)
+                base = (char) Math.max(base, chs[index] + 1);
+        }
+
         boolean found = false;
         for (char ch = base; ch <= '9'; ch++) {
             if (!incOrder && ch > chs[index])

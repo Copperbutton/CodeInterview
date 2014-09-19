98488356f4cea5e1d674715ed96f9bfdc2de40fd
Copperbutton <buptzxk2012@gmail.com>
2014-09-18 23:24:25 -0700

    Solved Find Top K records problem with Bianry Max Heap

---
+++ FindTopKRecordsII.java
@@ -0,0 +1,73 @@
+package interviewquestions.other;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Code interview questions from Yelp.
+ * Time: 17/9/2014
+ * Loc: CMU, PIT
+ */
+
+public class FindTopKRecords {
+    private class ItemVisit implements Comparable<ItemVisit> {
+        private String item;
+        private int visit;
+
+        public ItemVisit(String item, int visit) {
+            this.item = item;
+            this.visit = visit;
+        }
+
+        public int compareTo(ItemVisit item) {
+            if (item == null)
+                return 1;
+            return item.visit - this.visit;
+        }
+    }
+    
+    private List<String> readLogs(String filename) throws IOException {
+        List<String> logList = new ArrayList<String> ();
+        FileReader reader = new FileReader(filename);
+        BufferedReader buff = new BufferedReader(reader);
+        String logLine = buff.readLine();
+        while (logLine != null) {
+            logList.add(logLine);
+            logLine = buff.readLine();
+        }
+        buff.close();
+        return logList;
+    }
+
+    public List<String> findTopKRecords(List<String> logList, int K) {
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        BinaryMaxHeap<ItemVisit> heap = new BinaryMaxHeap<ItemVisit>();
+        List<String> topRecords = new ArrayList<String>();
+        for (String log : logList) {
+            String[] tokens = log.split("\t");
+            // If tokens.length < N
+            // log.add("Error: log format error" + log);
+            // continue;
+            String item = fetchItemFromSearchURL(tokens[3]);
+            int count = 1;
+            if (map.containsKey(item)) {
+                count = map.get(item) + 1;
+            }
+            map.put(item, count);
+        }
+
+        for (String item : map.keySet()) {
+            heap.add(new ItemVisit(item, map.get(item)));
+        }
+
+        for (int i = 0; i < K; i++) {
+            topRecords.add(heap.remove().item);
+        }
+        return topRecords;
+    }
+
+    private String fetchItemFromSearchURL(String URL) {
+        //To be implemented
+        return new String();
+    }
+}

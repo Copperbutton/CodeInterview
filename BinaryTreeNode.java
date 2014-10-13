473ca91fc8a8ca4083ca1151cf1180111fedea85
Copperbutton <buptzxk2012@gmail.com>
2014-10-12 21:09:12 -0700

    Binary Tree Node

---
+++ BinaryTreeNode.java
@@ -0,0 +1,11 @@
+package interviewquestions.google;
+
+public class BinaryTreeNode<T> {
+    public T val;
+    public BinaryTreeNode<T> left;
+    public BinaryTreeNode<T> right;
+    
+    public BinaryTreeNode(T val) {
+        this.val = val;
+    }
+}
